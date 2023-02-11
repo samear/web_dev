3c55ce8d747d885d89a2ba68b142ab5f855727f3
sam ear <sam.ear@gmail.com>
2023-02-10 21:04:12 -0800

    testing ajaxUtils

--- fullstack-course4/week5/lecture58_processing_JSON/js/script.js
+++ fullstack-course4/week5/lecture58_processing_JSON/js/script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded",
                     message += " doesn't like Chinese food";
                 }
                 message += " and uses ";
-                message += res.numberOfDisplays + 1;
+                message += res.numberOfDisplays + 11;
                 message += " displays for coding.";
                 document.querySelector("#content").innerHTML = "<h2>" + message + "</h2";
             });
